5165c99c20d93585bca4df5708076b55974fc943
mohamed bushra <m.bushra9577@gmail.com>
2024-12-20 12:45:05 +0400

    feature: Implement Pagination in UI

--- client/src/app/features/shop/shop.component.ts
+++ client/src/app/features/shop/shop.component.ts
@@ -8,9 +8,11 @@ import { ProductItemComponent } from "./product-item/product-item.component";
 import { MatButton } from '@angular/material/button';
 import { FiltersDialogComponent } from './filters-dialog/filters-dialog.component';
 import { MatIcon } from '@angular/material/icon';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import {MatMenu, MatMenuTrigger} from '@angular/material/menu'
 import { MatListOption, MatSelectionList, MatSelectionListChange } from '@angular/material/list';
 import { ShopeParams } from '../../shared/models/shopParams';
+import { Pagination } from '../../shared/models/pagination';
 
 @Component({
   selector: 'app-shop',
@@ -23,7 +25,8 @@ import { ShopeParams } from '../../shared/models/shopParams';
     MatMenu,
     MatSelectionList,
     MatListOption,
-    MatMenuTrigger
+    MatMenuTrigger,
+    MatPaginator
 ],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.scss'
@@ -34,7 +37,7 @@ export class ShopComponent implements OnInit{
   title = 'Skinet';
   private dialogService = inject(MatDialog);
 
-  products:Product[] = [];
+  products?:Pagination<Product>;
 
   sortOptions = [
     { name: 'Alphabetical' , value:'name'},
@@ -43,6 +46,7 @@ export class ShopComponent implements OnInit{
   ]
   
   shopParams = new ShopeParams;
+  pageSizeOptions = [5,10,15,20]
 
 
   ngOnInit(): void {
@@ -58,16 +62,23 @@ export class ShopComponent implements OnInit{
 
   getProducts(){
     this.shopeService.getProducts(this.shopParams).subscribe({
-      next: response => this.products = response.data,
+      next: response => this.products = response, //get paginated response 
       error: error => console.log(error),
     
     })
+  }
+
+  handelPageEvent(event: PageEvent){
+    this.shopParams.pageNumber = event.pageIndex + 1 ;
+    this.shopParams.pageSize =event.pageSize;
+    this.getProducts();
   }
 
   onSortChange(event:MatSelectionListChange){
      const selectedOption = event.options[0];  // grap the first element 
      if(selectedOption){
       this.shopParams.sort = selectedOption.value;
+      this.shopParams.pageNumber = 1;
      this.getProducts();
       
      }
@@ -89,6 +100,7 @@ export class ShopComponent implements OnInit{
           console.log(result);
           this.shopParams.brands = result.selectedBrands;
           this.shopParams.types = result.selectedTypes;
+          this.shopParams.pageNumber = 1 ;
           //apply filters
             this.getProducts();
           
